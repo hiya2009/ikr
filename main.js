b7c1e00becff289ef0009d02cb1ca6a66ead161c
hiya2009 <84074380+hiya2009@users.noreply.github.com>
2021-07-07 17:01:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload()
+{
+     song = loadSound("music.mp3");
+     song = loadSound("music2.mp3")
+} 
+function setup()
+{ 
+    canvas = createCanvas(600, 500);
+   canvas.center(); 
+   video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.posenNet(video, modelLoaded)
+    poseNet.on('pose',gotPoses);
+   }
